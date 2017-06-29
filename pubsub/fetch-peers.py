2218228b53b6cd84c03a7b89322d8ecb73759b6e
Jesus Velazquez <jesus.velazquez@gmail.com>
2017-06-29 07:49:02 -0700

    version1: basic funtionality
    Signed-off-by: Jesus Velazquez <jesus.velazquez@gmail.com>

--- pubsub/fetch-peers.py
+++ pubsub/fetch-peers.py
@@ -10,87 +10,127 @@ import logging
 import argparse
 import json
 
+class Subscriber:
+        MAX_MESSAGES=1024
 
-class AddressSubscriber:
-        MAX_MESSAGES = 128
+        def __init__(self, logger, subs):
+                self.subs = subs
+                self.logger = logger
+                self.name = subs.full_name.rsplit('/', 1)[-1]
 
+                self.log().info('Subscriber: %s created' % (self.name))
+
+        def log(self):
+                return self.logger
+
+        def pull(self):
+                results = self.subs.pull(return_immediately=True, max_messages=self.MAX_MESSAGES)
+                self.log().info('Sub:%s received %d messages' % (self.name, len(results)))
+
+                latest = None
+                ack_ids = []
+                for ack_id, msg in results:
+                        self.log().debug('* %s: %s' % (msg.message_id, msg.data))
+                        ack_ids.append(ack_id)
+
+                        next = json.loads(msg.data)
+                        if not latest:
+                                latest = next
+                                continue
+                        if int(latest['date']) < int(next['date']):
+                                latest = next
+
+                if len(ack_ids):
+                        self.subs.acknowledge(ack_ids)
+
+                        str = strftime('%Y-%m-%d %H:%M:%S', localtime(int(latest['date']) / 1000))
+                        self.log().info('Latest pulled: %s ip: %s' % (str, latest['ip']))
+
+                return latest
+
+        def getname(self):
+                return str(self.name)
+
+class MappingInfo:
         def __init__(self, hostname):
+                self.hostname = hostname
 
-                logging.basicConfig(level=logging.DEBUG)
-                logger = logging.getLogger(__name__)
+                logging.basicConfig(level=logging.INFO)
+                logger = logging.getLogger()
 
                 self.logger = logger
                 self.hostname = hostname
                 self.client = pubsub.Client()
+                self.subscribers = []
+                self.state_name = '%s.state' % (self.hostname)
 
-                logger.info('Fetching for {}'.format(self.hostname))
+        def log(self):
+                return self.logger
 
-        def load_topics(self):
+        def _pull_subs(self):
 
-                topics = []
-                topics_name = []
+                subscribers = []
                 for topic in self.client.list_topics():
-                        name = topic.full_name.rsplit('/', 1)[-1]
-                        if name != hostname:
-                                topics.append(topic)
-                                topics_name.append(name)
+                        topic_name = topic.full_name.rsplit('/', 1)[-1]
+                        if topic_name != hostname:
+                                self.log().info('Topic: %s found' % (topic_name))
 
-                self.topics = topics
-                self.logger.info('List of topics: {}'.format(topics_name))
+                                subs = topic.subscription(self.hostname)
+                                if not subs.exists():
+                                        self.log().warning('Topic: %s does not have subscription %s' %
+                                                      (topic_name, self.hostname))
+                                        continue
+                                subscribers.append(Subscriber(self.logger, subs))
 
-        def fetch(self):
+                self.subscribers = subscribers
 
-                for topic in self.topics:
-                        self._fetch_topic(topic)
-
-        def _fetch_topic(self, topic):
-
-                subs = topic.subscription(self.hostname)
-                topic_name = topic.full_name.rsplit('/', 1)[-1]
-
-                if not subs.exists():
-                        self.logger.error('Sub:{} does not exists for topic {}'.format(self.hostname, topic.full_name))
-                        return
-
-                results = subs.pull(return_immediately=True, max_messages=self.MAX_MESSAGES)
-                self.logger.info('Sub:{} received {} messages.'.format(subs.full_name, len(results)))
-
-                state_name = '{}.{}.state'.format(self.hostname, topic_name)
-                cur = None
-
+        def load(self):
+                self.log().info('Loading previous state...')
                 try:
-                        with open(state_name, 'r') as infile:
-                                cur = json.load(infile)
-                except IOError:
-                        self.logger.error('{} can\'t be opened, discarding old content'.format(state_name))
+                        with open(self.state_name, 'r') as f:
+                                self.mappings = json.loads(f.read())
+                        self.log().info('loaded mappings: %s' % self.mappings)
 
-                updated = False
+                except:
+                        self.log().info('Previuos state %s could not be loaded' % self.state_name)
+                        self.mappings = json.loads('{ "subs": { } }')
 
-                ack_ids = []
-                for ack_id, message in results:
-                        self.logger.debug('* {}: {}'.format(message.message_id, message.data))
-                        ack_ids.append(ack_id)
 
-                        next = json.loads(message.data)
-                        if not cur:
-                                cur = next;
-                                updated = True
-                                continue
+        def save(self):
+                self.log().info('Saving new state...')
+                try:
+                        with open(self.state_name, 'w') as f:
+                                json.dump(self.mappings, f, sort_keys=True, indent=4)
+                except Exception as e:
+                        self.log().error('Can\'t save state %s, mappings will be lost: %s' % (self.state_name, e))
 
-                        if int(cur['date']) < int(next['date']):
-                                cur = next
-                                updated = True
+        def pull(self):
 
-                if len(ack_ids):
-                        subs.acknowledge(ack_ids)
+                self.log().info('Pulling information...');
+                self._pull_subs()
 
-                        str = strftime('%Y-%m-%d %H:%M:%S', localtime(int(cur['date']) / 1000))
-                        self.logger.info('Latest update: {} ip: {}'.format(str, cur['ip']))
+                # Drain the queue (pull messages until there are not new)
+                while True:
+                        retry = False
+                        for subscriber in self.subscribers:
+                                latest = subscriber.pull()
+                                if latest:
+                                        name = subscriber.getname()
+                                        if name in self.mappings["subs"]:
+                                                current = self.mappings["subs"][name]
+                                                if int(current['date']) > int(latest['date']):
+                                                        self.log().info('Current is newer than latest: %s' % (name))
+                                                        latest = current
 
-                        if updated:
-                                self.logger.info('Updating {}'.format(state_name))
-                                with open('{}.{}.state'.format(self.hostname, topic_name), 'w') as outfile:
-                                        json.dump(cur, outfile)
+                                        self.mappings["subs"][name] = latest
+                                        str = strftime('%Y-%m-%d %H:%M:%S', localtime(int(latest['date']) / 1000))
+                                        self.log().info('Updating %s: %s ip: %s' % (name, str, latest['ip']))
+                                        retry = True
+                                                
+                                
+                        if not retry:
+                                break
+                
 
 if __name__ ==  '__main__':
 
@@ -104,6 +144,8 @@ if __name__ ==  '__main__':
         if args.hostname:
                 hostname = args.hostname
 
-        Addr = AddressSubscriber(hostname)
-        Addr.load_topics()
-        Addr.fetch()
+        mappings = MappingInfo(hostname)
+
+        mappings.load()
+        mappings.pull()
+        mappings.save()
